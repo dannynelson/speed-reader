a87f734eb0f93d56fc1488c7a0a50889c02d5c42
Daniel Nelson <danielnelsonguitar@gmail.com>
2013-12-16 11:19:00 -0800

    Text only wrapped on alt click

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 // alert('script worked!');
 console.log('hello');
 
-$('p').each(function(idx, item) {
-  $node = $(item);
-  var words = $node.text().split(' ');
-  $node.text('');
-  words.forEach(function(word) {
-    $node.append('<span>' + word + '</span> ');
-  });
-});
-
-$('span').on('click', function() {
-  $(this).addClass('sr-selected');
-});
-
-$(document).keydown(function(e){
-  if (e.keyCode == 37) {}
-  if (e.keyCode == 39) {
-    $next = $('.sr-selected').next();
-    $('.sr-selected').removeClass('sr-selected');
-    $next.addClass('sr-selected');
+$('p').on('click', function(event){
+  if (event.altKey) {
+    console.log('altClicked!');
+    wrapTextInSpans();
   }
 });
+
+var wrapTextInSpans = function() {
+  $('p').each(function(idx, item) {
+    $node = $(item);
+    var words = $node.text().split(' ');
+    $node.text('');
+    words.forEach(function(word) {
+      $node.append('<span class="sr">' + word + '</span> ');
+    });
+  });
+};
+
+
+// $('span').on('click', function() {
+//   $(this).addClass('sr-selected');
+// });
+
+// $(document).keydown(function(e){
+//   debugger;
+//   if (e.keyCode == 37) {
+//     $prev = $('.sr-selected').prev();
+//     $('.sr-selected').removeClass('sr-selected');
+//     $prev.addClass('sr-selected');
+//   }
+//   if (e.keyCode == 39) {
+//     $next = $('.sr-selected').next();
+//     $('.sr-selected').removeClass('sr-selected');
+//     $next.addClass('sr-selected');
+//   }
+// });
